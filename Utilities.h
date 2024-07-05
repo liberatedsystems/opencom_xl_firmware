a014d2593778153e09aa0bec015a70ee9c16e6c7
jacob.eva <x00010@disroot.org>
2024-07-05 11:12:10 +0100

    Add LED dimming to suit case

--- Utilities.h
+++ Utilities.h
@@ -206,10 +206,10 @@ uint8_t boot_vector = 0x00;
 	#endif
 #elif MCU_VARIANT == MCU_NRF52
     #if BOARD_MODEL == BOARD_RAK4631
-		void led_rx_on()  { digitalWrite(pin_led_rx, HIGH); }
-		void led_rx_off() {	digitalWrite(pin_led_rx, LOW); }
-		void led_tx_on()  { digitalWrite(pin_led_tx, HIGH); }
-		void led_tx_off() { digitalWrite(pin_led_tx, LOW); }
+		void led_rx_on()  { analogWrite(pin_led_rx, 1); }
+		void led_rx_off() {	analogWrite(pin_led_rx, 0); }
+		void led_tx_on()  { analogWrite(pin_led_tx, 1); }
+		void led_tx_off() { analogWrite(pin_led_tx, 0); }
     #endif
 #endif
 
